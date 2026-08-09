1a4fe064b0822f5bbc00fea318ca1bc0e49fcaf7
DoMe <1812020+djdomi@users.noreply.github.com>
2026-08-09 13:47:02 +0200

    CDX-1786275200 fix(setup): add required SPDX header

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# SPDX-License-Identifier: AGPL-3.0-or-later
 # lancache-ng (https://github.com/wiki-mod/lancache-ng)
 #
 # Guided lifecycle CLI for a lancache-ng installation. Subcommands: install
